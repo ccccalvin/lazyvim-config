33f63e35a90c28fd11e12a002e27020677fbb16e
cvn-arch-wsl <calvintkusnadi@gmail.com>
2025-06-23 22:19:21 +1000

    commit msg

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- add transparency
+-- vim.opt.termguicolors = true
+-- vim.opt.background = "dark"
+-- vim.cmd([[
+--   highlight Normal     guibg=none ctermbg=none
+--   highlight NormalNC   guibg=none ctermbg=none
+--   highlight SignColumn guibg=none ctermbg=none
+--   highlight VertSplit  guibg=none ctermbg=none
+--   highlight EndOfBuffer guibg=none ctermbg=none
+-- ]])
