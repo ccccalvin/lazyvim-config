f3890fcaba97739d347f21df1feb6ba80247f550
z5588713 <z5588713@vx16.orchestra.cse.unsw.EDU.AU>
2025-07-01 10:45:44 +1000

    added 4 space indenting for C files

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c",
+  callback = function()
+    local o = vim.opt_local
+    o.expandtab = true
+    o.shiftwidth = 4
+    o.tabstop = 4
+    o.softtabstop = 4
+  end,
+})
