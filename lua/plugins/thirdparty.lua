0af10be0007d5d001daba98878456aa2da6bb415
Calvin Kusnadi <z5588713@ad.unsw.edu.au>
2025-07-15 13:25:08 +1000

    added lazygit plugin

--- lua/plugins/thirdparty.lua
+++ lua/plugins/thirdparty.lua
@@ -12,4 +12,19 @@ return {
       })
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<Leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 }
